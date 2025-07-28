d418a7bfc18225f7689d3413908652370881d6b5
hannannexus <hannannexus@gmail.com>
2025-07-28 16:39:12 +0600

    add textdoamin at escaping

--- inc/menu-pages/settings.php
+++ inc/menu-pages/settings.php
@@ -1,5 +1,5 @@
 <div class="wrap cmfw-admin">
-    <h1><?php echo esc_html('Custom Meta Settings'); ?></h1>
+    <h1><?php echo esc_html('Custom Meta Settings','custom-meta-for-woocommerce'); ?></h1>
     <?php settings_errors(); ?>
 
     <form method="post" action="options.php">
@@ -7,52 +7,52 @@
 
         <div class="woo-cmfw-settings-wrap">
             <div class="woo-cmfw-settings-nav">
-                <button type="button" class="nav-tab" data-target="tab-general"><?php echo esc_html('General'); ?></button>
-                <button type="button" class="nav-tab" data-target="tab-design"><?php echo esc_html('Design'); ?></button>
+                <button type="button" class="nav-tab" data-target="tab-general"><?php echo esc_html(__('General', 'custom-meta-for-woocommerce')); ?></button>
+                <button type="button" class="nav-tab" data-target="tab-design"><?php echo esc_html(__('Design', 'custom-meta-for-woocommerce')); ?></button>
             </div>
             <div class="woo-cmfw-settings-content">
                 <div id="tab-general" class="tab-content">
-                    <h2><?php echo esc_html('General Settings'); ?></h2>
+                    <h2><?php echo esc_html(__('General Settings', 'custom-meta-for-woocommerce')); ?></h2>
                     <table class="form-table">
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Product Faq'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Product Faq', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->ProductFaq(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Faq Position'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Faq Position', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->faqPosition(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Faq Heading'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Faq Heading', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->Heading(); ?></td>
                         </tr>
                     </table>
                 </div>
                 <div id="tab-design" class="tab-content">
-                    <h2><?php echo esc_html('Design Settings'); ?></h2>
+                    <h2><?php echo esc_html(__('Design Settings', 'custom-meta-for-woocommerce')); ?></h2>
                     <table class="form-table">
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Heading Font Color'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Heading Font Color', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->HeadingColor(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Question Font Color'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Question Font Color', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->QuestionColor(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Answer Font Color'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Answer Font Color', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->AnswerColor(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Heading Font Size'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Heading Font Size', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->HeadingFontSize(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Question Font Size'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Question Font Size', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->QuestionFontSize(); ?></td>
                         </tr>
                         <tr valign="top">
-                            <th scope="row"><?php echo esc_html('Answer Font Size'); ?></th>
+                            <th scope="row"><?php echo esc_html(__('Answer Font Size', 'custom-meta-for-woocommerce')); ?></th>
                             <td><?php //$menu_instance->AnswerFontSize(); ?></td>
                         </tr>
                     </table>
