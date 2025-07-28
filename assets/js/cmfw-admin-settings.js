3f3daf5fe15e7f8daaecbc677973682113fcf2d9
hannannexus <hannannexus@gmail.com>
2025-07-28 16:24:57 +0600

    change class name  at settings.php and cmfw-admin-settings.js file

--- assets/js/cmfw-admin-settings.js
+++ assets/js/cmfw-admin-settings.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($) {
     // Tab functionality
-    $('.woo-faq-settings-nav .nav-tab').on('click', function(e) {
+    $('.woo-cmfw-settings-nav .nav-tab').on('click', function(e) {
         e.preventDefault();
 
         // Deactivate all tabs and content
-        $('.woo-faq-settings-nav .nav-tab').removeClass('nav-tab-active');
-        $('.woo-faq-settings-content .tab-content').removeClass('active');
+        $('.woo-cmfw-settings-nav .nav-tab').removeClass('nav-tab-active');
+        $('.woo-cmfw-settings-content .tab-content').removeClass('active');
 
         // Activate the clicked tab and its content
         $(this).addClass('nav-tab-active');
