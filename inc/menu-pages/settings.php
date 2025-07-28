b01c65f65da5fd29f4b2c1f644cbad8165694bc8
hannannexus <hannannexus@gmail.com>
2025-07-28 16:33:43 +0600

    commented previous method

--- inc/menu-pages/settings.php
+++ inc/menu-pages/settings.php
@@ -3,7 +3,7 @@
     <?php settings_errors(); ?>
 
     <form method="post" action="options.php">
-        <?php settings_fields('woofaq-settings-group'); ?>
+        <?php //settings_fields('woofaq-settings-group'); ?>
 
         <div class="woo-cmfw-settings-wrap">
             <div class="woo-cmfw-settings-nav">
@@ -16,15 +16,15 @@
                     <table class="form-table">
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Product Faq'); ?></th>
-                            <td><?php $menu_instance->ProductFaq(); ?></td>
+                            <td><?php //$menu_instance->ProductFaq(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Faq Position'); ?></th>
-                            <td><?php $menu_instance->faqPosition(); ?></td>
+                            <td><?php //$menu_instance->faqPosition(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Faq Heading'); ?></th>
-                            <td><?php $menu_instance->Heading(); ?></td>
+                            <td><?php //$menu_instance->Heading(); ?></td>
                         </tr>
                     </table>
                 </div>
@@ -33,27 +33,27 @@
                     <table class="form-table">
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Heading Font Color'); ?></th>
-                            <td><?php $menu_instance->HeadingColor(); ?></td>
+                            <td><?php //$menu_instance->HeadingColor(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Question Font Color'); ?></th>
-                            <td><?php $menu_instance->QuestionColor(); ?></td>
+                            <td><?php //$menu_instance->QuestionColor(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Answer Font Color'); ?></th>
-                            <td><?php $menu_instance->AnswerColor(); ?></td>
+                            <td><?php //$menu_instance->AnswerColor(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Heading Font Size'); ?></th>
-                            <td><?php $menu_instance->HeadingFontSize(); ?></td>
+                            <td><?php //$menu_instance->HeadingFontSize(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Question Font Size'); ?></th>
-                            <td><?php $menu_instance->QuestionFontSize(); ?></td>
+                            <td><?php //$menu_instance->QuestionFontSize(); ?></td>
                         </tr>
                         <tr valign="top">
                             <th scope="row"><?php echo esc_html('Answer Font Size'); ?></th>
-                            <td><?php $menu_instance->AnswerFontSize(); ?></td>
+                            <td><?php //$menu_instance->AnswerFontSize(); ?></td>
                         </tr>
                     </table>
                 </div>
