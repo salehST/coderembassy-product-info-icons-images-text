cbb8de5995821d51c28475ee7e9e54881fb26c31
hannannexus <hannannexus@gmail.com>
2025-09-23 22:12:12 +0600

    add float rule

--- inc/menu-pages/dashboard.php
+++ inc/menu-pages/dashboard.php
@@ -267,7 +267,7 @@ if ($enable_meta !== '1') {
                     foreach ($saved_groups as $group_index => $group_data) {
                         echo '<div class="cmfw-group cmfw-group-wrap" data-group-index="' . esc_attr($group_index) . '">';
                         if ($group_index === 0) {
-                            echo '<h2 class="header_sction">' . esc_html__('Product Info Group', 'coderembassy-product-info-icons-images-text') . ' ' . ($group_index + 1) . ' <span style="color: #666; font-size: 0.8em;">(' . esc_html__('Free Version - Required', 'coderembassy-product-info-icons-images-text') . ')</span></h2>';
+                            echo '<h2 class="header_sction">' . esc_html__('Product Info Group', 'coderembassy-product-info-icons-images-text') . ' ' . ($group_index + 1) . ' <span style="color: #666; font-size: 0.8em;">' . esc_html__('', 'coderembassy-product-info-icons-images-text') . '</span></h2>';
                         } else {
                             echo '<h2 class="header_sction">' . esc_html__('Product Info Group', 'coderembassy-product-info-icons-images-text') . ' ' . ($group_index + 1) . ' <span style="color: #0073aa; font-size: 0.8em;">(' . esc_html__('PRO Version', 'coderembassy-product-info-icons-images-text') . ')</span></h2>';
                         }
@@ -309,7 +309,7 @@ if ($enable_meta !== '1') {
             </div>
             <hr>
             <input type="submit" name="save_cmfw" class="button button-primary" value="<?php echo esc_attr__('Save', 'coderembassy-product-info-icons-images-text'); ?>">
-            <p class="description"><?php echo esc_html__('Click Save to save your Product Info data. No validation required.', 'coderembassy-product-info-icons-images-text'); ?></p>
+            <p class="description"><?php //echo esc_html__('Click Save to save your Product Info data. No validation required.', 'coderembassy-product-info-icons-images-text'); ?></p>
         </form>
         
     </div>
